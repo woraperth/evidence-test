fd114d19622b501ce9a3f281f85c05002d96d3c8
Perth <perth.ngarmtrakulchol@servian.com>
2026-07-09 00:20:35 +1000

    Update header

---
+++ pages/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import '@evidence-dev/tailwind/fonts.css';
+	import '../app.css';
+	import { EvidenceDefaultLayout } from '@evidence-dev/core-components';
+	export let data;
+</script>
+
+<EvidenceDefaultLayout {data} title="Road to Data Engineer by DataTH">
+	<slot slot="content" />
+</EvidenceDefaultLayout>
